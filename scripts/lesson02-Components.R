3e1297b2cad7d72036b435f2a278f213f0de0e0d
belinskyc <belinsky@msu.edu>
2026-04-21 13:57:39 -0400

    thhis í my commit

--- scripts/lesson02-Components.R
+++ scripts/lesson02-Components.R
@@ -1,4 +1,4 @@
-rm(list=ls());                         # clear Environment tab
+rm(list=ls());                         # clear Environment tabfdsdfhgfd
 library(package=ggplot2);              # get the GGPlot package
 
 # read in CSV file and save the content to weatherData
